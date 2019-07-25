f634b11292b98039fbf1e147e44cc2ba3c7672d1
Vitalii Nepochatov <vitalii.nepochatov@gmail.com>
2019-07-26 02:29:52 +0300

    Cache layer. Added: logout, get account, save account

--- app/src/main/java/info/fandroid/chat/cache/AccountCacheImpl.kt
+++ app/src/main/java/info/fandroid/chat/cache/AccountCacheImpl.kt
@@ -1,9 +1,10 @@
 package info.fandroid.chat.cache
 
 import info.fandroid.chat.data.account.AccountCache
+import info.fandroid.chat.domain.account.AccountEntity
 import info.fandroid.chat.domain.type.Either
 import info.fandroid.chat.domain.type.None
-import info.fandroid.chat.domain.type.exception.Failure
+import info.fandroid.chat.domain.type.Failure
 import javax.inject.Inject
 
 class AccountCacheImpl @Inject constructor(private val prefsManager: SharedPrefsManager) : AccountCache {
@@ -14,5 +15,17 @@ class AccountCacheImpl @Inject constructor(private val prefsManager: SharedPrefs
 
     override fun getToken(): Either<Failure, String> {
         return prefsManager.getToken()
+    }
+
+    override fun logout(): Either<Failure, None> {
+        return prefsManager.removeAccount()
+    }
+
+    override fun getCurrentAccount(): Either<Failure, AccountEntity> {
+        return prefsManager.getAccount()
+    }
+
+    override fun saveAccount(account: AccountEntity): Either<Failure, None> {
+        return prefsManager.saveAccount(account)
     }
 }
