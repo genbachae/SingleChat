dd67ba22e90d8842267c9962cafeef2356aafe14
Vitalii Nepochatov <vitalii.nepochatov@gmail.com>
2019-10-17 11:06:05 +0300

    Cache layer. Added: messages

--- app/src/main/java/info/fandroid/chat/cache/ChatDatabase.kt
+++ app/src/main/java/info/fandroid/chat/cache/ChatDatabase.kt
@@ -5,11 +5,14 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import info.fandroid.chat.cache.friends.FriendsDao
+import info.fandroid.chat.cache.messages.MessagesDao
 import info.fandroid.chat.domain.friends.FriendEntity
+import info.fandroid.chat.domain.messages.MessageEntity
 
-@Database(entities = [FriendEntity::class], version = 2, exportSchema = false)
+@Database(entities = [FriendEntity::class, MessageEntity::class], version = 5, exportSchema = false)
 abstract class ChatDatabase : RoomDatabase() {
     abstract val friendsDao: FriendsDao
+    abstract val messagesDao: MessagesDao
 
     companion object {
         @Volatile
